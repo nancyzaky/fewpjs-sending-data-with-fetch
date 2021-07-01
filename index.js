11ebf5e4b88e905bfb8677aa8d313d86ef1b9021
Nancy Zaky <nancyzki@yahoo.com>
2021-06-30 22:01:12 -0700

    commit tree a76b8dbfbee87f79392c519c46792c42927a7c56

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+const submitData= (name, email) => {
+  let obj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }, body:
+      JSON.stringify({name, email})
+
+  }
+  return fetch('http://localhost:3000/users', obj).then(resp => resp.json())
+  .then(function(obj) {
+    console.log(obj.id)
+    let node = document.createElement("p")
+    let textnode = document.createTextNode(obj.id)
+    node.appendChild(textnode)
+
+
+    const newId = document.querySelector('body');
+    newId.appendChild(node);
+  })
+  .catch(error => {
+    const errorMessage = error.message;
+    let node = document.createElement('p');
+    let textNode = document.createTextNode(errorMessage);
+    node.appendChild(textNode);
+    const newErrorMessage = document.querySelector('section#error');
+    newErrorMessage.appendChild(node)
+  })
+}
+submitData('hdjb', 'nahnekbdjvn');
